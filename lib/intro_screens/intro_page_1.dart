28c3ce0c09295ef4366ef19bd51909df7ca79498
seymatopuz1 <stopuz746@gmail.com>
2022-08-12 16:25:20 +0300

    flutter login information 0.1

---
+++ lib/intro_screens/intro_page_1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IntroPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Colors.pink[100],
+      child: const Center(
+        child: Text('Page 1'),
+      ),
+    )
+
+        // put images and cool stuff here to describe what your app does
+        );
+  }
+}
